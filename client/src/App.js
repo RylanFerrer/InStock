5494bee216e11a6d6f21b01169e4ef49b2952c9b
russellwinters <russellwinters@protonmail.com>
2019-11-19 13:55:40 -0800

    table is styled on desktop

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import CreateNew from './components/CreateNew'
-import "./styles/main.css"
+import CreateNew from "./components/CreateNew";
+import "./styles/main.css";
+import Inventory from "./components/product-display/Inventory";
 export default function App() {
   return (
-      <div className="App">
-        <h1>Hello riri</h1>
-        <CreateNew/>
-      </div>
-  )
+    <div className="App">
+      <h1>Hello riri</h1>
+      {/* <CreateNew/> */}
+      <Inventory />
+    </div>
+  );
 }
-
