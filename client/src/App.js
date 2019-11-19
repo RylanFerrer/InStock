ad8dbce4b75b94421973f4e1a1995b6bc6318818
Ivy Mageto <ivymageto@Ivys-MacBook-Pro.local>
2019-11-18 21:59:43 -0800

    added links to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello riri</h1>
+      <Navbar />
     </div>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,22 @@
 import React, { Component } from "react";
+import Logo from "../assets/Logo/Logo-instock.svg";
 
 export default function Navbar() {
-  return <header></header>;
+  return (
+    <header>
+      <div className="header-logo">
+        <a href="index.html">
+          <img className="header__image" src={Logo} alt="Instock Logo" />
+        </a>
+      </div>
+      <nav className="nav-container">
+        <a className="header__nav" href="#">
+          Inventory
+        </a>
+        <a className="header__nav" href="#">
+          Locations
+        </a>
+      </nav>
+    </header>
+  );
 }
