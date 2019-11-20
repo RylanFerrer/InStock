6e71f20190561c951dbb9a3b30a86e7f2663ed20
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-20 14:00:25 -0800

    added backend to locations

--- client/src/components/locations.js
+++ client/src/components/locations.js
@@ -1,49 +1,62 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-class Locations extends React.Component {
-  render() {
-    const locationsInfo = this.props.locationsInfo;
-    console.log(locationsInfo);
-    const locationsSection = locationsInfo.map(locs => {
-      return (
-        <div className="locations-content">
-          <div className="warehouse-container">
-            <div className="warehouse-object1">{locs.warehouse1}</div>
-            <div className="warehouse-object2">{locs.warehouse2}</div>
-          </div>
-          <div className="contact-container">
-            <div className="contact-object">{locs.contact}</div>
-            <div className="contact-info__object">{locs.contactinfo}</div>
-            <div className="category-object">{locs.categories}</div>
-          </div>
-        </div>
-      );
+export default class Locations extends React.Component {
+  state = {
+    locationsInfo: []
+  };
+
+  componentDidMount() {
+    console.log("component mounted");
+    axios.get("http://localhost:5000/locations").then(answer => {
+      this.setState({ locationsInfo: answer.data });
     });
+  }
 
-    return (
-      <section className="locations-container">
-        <div className="locations-div">
-          <h1 className="locations-title">Locations</h1>
-          <input
-            className="header-input"
-            type="text"
-            name="name"
-            placeholder="Search"
-          />
-        </div>
+  render() {
+    if (this.state.locationsInfo.length > 0) {
+      console.log(this.state.locationsInfo);
+      let test = this.state.locationsInfo.map(obj => {
+        return (
+          <>
+            <div className="locations-content">
+              <div className="warehouse-container">
+                <div className="warehouse-object1">{obj.warehouse1}</div>
+                <div className="warehouse-object2">{obj.warehouse2}</div>
+              </div>
+              <div className="contact-container">
+                <div className="contact-object">{obj.contact}</div>
+                <div className="contact-info__object">{obj.contactinfo}</div>
+                <div className="category-object">{obj.categories}</div>
+              </div>
+            </div>
+          </>
+        );
+      });
+      return (
+        <section className="locations-container">
+          <div className="locations-div">
+            <h1 className="locations-title">Locations</h1>
+            <input
+              className="header-input"
+              type="text"
+              name="name"
+              placeholder="Search"
+            />
+          </div>
 
-        <div className="locations-keys">
-          <span className="locations-keys__warehouse">WAREHOUSE</span>
-          <span className="locations-keys__contact">CONTACT</span>
-          <span className="locations-keys__contactinfo">
-            CONTACT INFORMATION
-          </span>
-          <span className="locations-keys__categories">CATEGORIES</span>
-        </div>
-        {locationsSection}
-      </section>
-    );
+          <div className="locations-keys">
+            <span className="locations-keys__warehouse">WAREHOUSE</span>
+            <span className="locations-keys__contact">CONTACT</span>
+            <span className="locations-keys__contactinfo">
+              CONTACT INFORMATION
+            </span>
+            <span className="locations-keys__categories">CATEGORIES</span>
+          </div>
+          {test}
+        </section>
+      );
+    } else return <h1>Loading</h1>;
   }
 }
-
-export default Locations;
+// }
