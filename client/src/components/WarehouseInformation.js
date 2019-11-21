55b9259ebc36603762beb7394466ec4ddc86e162
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-21 14:49:09 -0800

    backend changes to the warehouse info

--- client/src/components/WarehouseInformation.js
+++ client/src/components/WarehouseInformation.js
@@ -1,129 +1,95 @@
 import React, { Component } from "react";
 import Arrow from "../assets/Icons/SVG/Icon-back-arrow.svg";
 import ProductTable from "./product-display/ProductTable";
+import axios from "axios";
 
 export default class WarehouseInformation extends Component {
-  render() {
-    const fakeWarehouse = {
-      street: "123 Main Street W.",
-      country: "canada",
-      city: "Toronto",
-      areacode: "M65GB",
-      contact: "Mara Weinberg",
-      role: "Warehouse Manager",
-      phone: "1 555 555 5555",
-      email: "weinberg@instock.com",
-      warehouse: "Noor's Cheetos "
-    };
+  state = {
+    warehouse: undefined,
+    products: undefined
+  };
 
-    const fakeProducts = [
-      [
-        {
-          item: "Wood",
-          description: "description of item",
-          ordered_by: "Mara Weinberg Warehouse Manager",
-          last_ordered: "5/24/2018",
-          quantity: "1200",
-          reference_number: "123456789",
-          location: "Toronto, ON",
-          status: "In Stock",
-          key: 123456,
-          warehouseID: 1234,
-          categories:
-            " Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation"
-        },
-        {
-          item: "Metal",
-          description: "this is goint to be wild",
-          ordered_by: "Mara Weinberg Warehouse Manager",
-          last_ordered: "5/24/2018",
-          quantity: "54000",
-          reference_number: "123456789",
-          location: "Toronto, ON",
-          status: "In Stock",
-          key: 99876,
-          warehouseID: 1234,
-          categories:
-            "Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation"
-        },
-        {
-          item: "Cars",
-          description: "yeah",
-          ordered_by: "Mara Weinberg Warehouse Manager",
-          last_ordered: "5/24/2018",
-          quantity: "54000",
-          reference_number: "542039",
-          location: "Toronto, ON",
-          status: "In Stock",
-          key: 99876,
-          warehouseID: 1234,
-          categories:
-            "Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation"
-        }
-      ]
-    ];
-    console.log(this.props);
-    return (
-      <>
-        <header className="header">
-          <div className="header-container">
-            <img className="header-container__image" src={Arrow} />
-            <h1 className="header-container__title">
-              {fakeWarehouse.warehouse}
-            </h1>
-          </div>
-        </header>
-        <section className="warehouse-info">
-          <div className="warehouse-info__content">
-            <span className="warehouse-info__content-key">Address</span>
-            <div className="warehouse-info__content-container">
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.street}
-              </span>
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.areacode}
-              </span>
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/locations/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          warehouse: res.data[0],
+          products: [res.data[1]]
+        });
+        console.log(this.state);
+      });
+  }
+
+  render() {
+    if (
+      this.state.warehouse !== undefined &&
+      this.state.products !== undefined
+    ) {
+      return (
+        <>
+          <header className="header">
+            <div className="header-container">
+              <img className="header-container__image" src={Arrow} />
+              <h1 className="header-container__title">
+                {this.state.warehouse[0].name}
+              </h1>
             </div>
-            <div className="warehouse-info__content-container">
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.city}
-              </span>
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.country}
-              </span>
+          </header>
+          <section className="warehouse-info">
+            <div className="warehouse-info__content">
+              <span className="warehouse-info__content-key">Address</span>
+              <div className="warehouse-info__content-container">
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].street}
+                </span>
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].areacode}
+                </span>
+              </div>
+              <div className="warehouse-info__content-container">
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].city}
+                </span>
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].country}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="warehouse-info__content">
-            <span className="warehouse-info__content-key">Contact</span>
-            <div className="warehouse-info__content-container">
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.contact}
-              </span>
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.role}
-              </span>
+            <div className="warehouse-info__content">
+              <span className="warehouse-info__content-key">Contact</span>
+              <div className="warehouse-info__content-container">
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].contact}
+                </span>
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].role}
+                </span>
+              </div>
+              <div className="warehouse-info__content-container">
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].phone}
+                </span>
+                <span className="warehouse-info__content-container--value">
+                  {this.state.warehouse[0].email}
+                </span>
+              </div>
             </div>
-            <div className="warehouse-info__content-container">
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.phone}
-              </span>
-              <span className="warehouse-info__content-container--value">
-                {fakeWarehouse.email}
-              </span>
+          </section>
+          <section className="warehouse-products">
+            <div className="inventory-keys">
+              <span className="inventory-keys__content">Item</span>
+              <span className="inventory-keys__content">Last Ordered</span>
+              <span className="inventory-keys__content">Location</span>
+              <span className="inventory-keys__content">Quantity</span>
+              <span className="inventory-keys__content">Status</span>
             </div>
-          </div>
-        </section>
-        <section className="warehouse-products">
-          <div className="inventory-keys">
-            <span className="inventory-keys__content">Item</span>
-            <span className="inventory-keys__content">Last Ordered</span>
-            <span className="inventory-keys__content">Location</span>
-            <span className="inventory-keys__content">Quantity</span>
-            <span className="inventory-keys__content">Status</span>
-          </div>
-          <ProductTable products={fakeProducts} />
-        </section>
-      </>
-    );
+            <ProductTable products={this.state.products} />
+          </section>
+        </>
+      );
+    } else {
+      return <h1>Loading</h1>;
+    }
   }
 }
--- server/routes/api/locations.js
+++ server/routes/api/locations.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const fileName = "../../models/locations.json";
+const productName = "../../models/products.json";
 const locations = require(fileName);
+const products = require(productName);
 
 //get locations data
 router.get("/", (req, res) => {
   res.json(locations);
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const warehouse = locations.filter(location => location.id === id);
+  const prod = products.filter(product => product.warehouseID === id);
+  res.json([warehouse, prod]);
+});
 module.exports = router;
